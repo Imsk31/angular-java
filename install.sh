6cd0848f1df685bc641dfafffcd8eb015f23333d
Imsk31 <121716970+Imsk31@users.noreply.github.com>
2024-09-15 14:14:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Add Docker's official GPG key
-sudo apt-get update -y
+sudo apt-get update
+sudo apt install unzip -y
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -14,7 +15,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package lists and install Docker
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install kubectl
